6f941e9bfe30d65c63c4855c305b0cc19fb8c7ad
Florian <florian@flova.de>
2021-04-27 17:11:25 +0200

    Add ipm

--- active_soccer_vision/sim/camera.py
+++ active_soccer_vision/sim/camera.py
@@ -9,9 +9,13 @@ class Camera:
         self.height = height
         self.width = width
 
-        T_cam = [5.0, 5.0, 1.0]
+        T_cam = [4.5, 3.0, 1.0]
         R_cam = transforms3d.euler.euler2mat(0, math.radians(45), 0)
         self.camera_frame = transforms3d.affines.compose(T_cam, R_cam, np.ones(3))
+
+        R_camera_optical_frame = [[0,0,1], [0, -1, 0], [1, 0, 0]]
+        self.camera_optical_frame = transforms3d.affines.compose(np.zeros(3), R_camera_optical_frame, np.ones(3))
+
 
         def mat_from_fov_and_resolution(fov, res):
             return 0.5 * res * (math.cos((fov / 2)) / math.sin((fov / 2)))
@@ -32,14 +36,20 @@ class Camera:
     def get_point_in_camera_frame(self, point_in_world_frame):
         return np.matmul(np.linalg.inv(self.camera_frame), point_in_world_frame)
 
+    def get_point_in_camera_optical_frame(self, point_in_world_frame):
+        point = self.get_point_in_camera_frame(point_in_world_frame)
+
+        return np.matmul(np.linalg.inv(self.camera_optical_frame), point)
+
     def check_if_point_is_visible(self, point):
-        A_ball_in_cam_frame = self.get_point_in_camera_frame(point)
 
-        T_camera_optical_frame = [1.0, 0, 0]
-        R_camera_optical_frame = [[0,0,1], [0, -1, 0], [1, 0, 0]]
-        A_camera_optical_frame = transforms3d.affines.compose(T_camera_optical_frame, R_camera_optical_frame, np.ones(3))
+        if point.shape[0] == 2:
+            point = np.append(point, 0)
 
-        A_ball_in_cam_optical_frame = np.matmul(np.linalg.inv(A_camera_optical_frame), A_ball_in_cam_frame)
+        R_point_on_map = transforms3d.euler.euler2mat(0.0, 0, 0)
+        A_point_on_map = transforms3d.affines.compose(point, R_point_on_map, np.ones(3))
+
+        A_ball_in_cam_optical_frame = self.get_point_in_camera_optical_frame(A_point_on_map)
 
         p, _, _, _ = transforms3d.affines.decompose(A_ball_in_cam_optical_frame)
         
@@ -48,17 +58,69 @@ class Camera:
             p_pixel = p_pixel * (1/p_pixel[2])
             
             if 0 < p_pixel[0] <= self.width and 0 < p_pixel[1] <= self.height:
+                print(self.get_pixel_position_in_world(np.array([p_pixel[0], p_pixel[1], 0])))
                 return True
         return False
+
+    def get_pixel_position_in_world(self, point): 
+        A_field_normal = transforms3d.affines.compose([0, 0, 1], np.eye(3), np.ones(3))
+        A_field_normal = self.get_point_in_camera_optical_frame(A_field_normal)
+
+        A_field_point = transforms3d.affines.compose(np.zeros(3), np.eye(3), np.ones(3))
+        A_field_point = self.get_point_in_camera_optical_frame(A_field_point)
+
+        field_normal, _, _ , _ = transforms3d.affines.decompose(A_field_normal)
+        field_point , _, _ , _ = transforms3d.affines.decompose(A_field_point)
+
+        field_normal = field_point - field_normal
+
+        point = self._get_field_intersection_for_pixels(np.array([[point[0], point[1], 0]]), (field_normal, field_point))[0]
+
+        point = transforms3d.affines.compose(point, np.eye(3), np.ones(3))
+
+        point = np.matmul(np.matmul(self.camera_frame, self.camera_optical_frame), point)
+
+        point, _, _ , _ = transforms3d.affines.decompose(point)
+
+        return point
+        
+        
+    def _get_field_intersection_for_pixels(self, points, field):
+        """
+        Projects an numpy array of points to the correspoding places on the field plane (in the camera frame).
+        """
+        camera_projection_matrix = self.K.reshape(-1)
+
+        points[:, 0] = (points[:, 0] - (camera_projection_matrix[2])) / camera_projection_matrix[0]
+        points[:, 1] = (points[:, 1] - (camera_projection_matrix[5])) / camera_projection_matrix[4]
+        points[:, 2] = 1
+
+        intersections = self._line_plane_intersections(field[0], field[1], points)
+
+        return intersections
+
+    def _line_plane_intersections(self, plane_normal, plane_point, ray_directions):
+        n_dot_u = np.tensordot(plane_normal, ray_directions, axes=([0],[1]))
+        relative_ray_distance = -plane_normal.dot(- plane_point) / n_dot_u
+
+        # we are casting a ray, intersections need to be in front of the camera
+        relative_ray_distance[relative_ray_distance <= 0] = np.nan
+
+        ray_directions[:,0] = np.multiply(relative_ray_distance, ray_directions[:,0])
+        ray_directions[:,1] = np.multiply(relative_ray_distance, ray_directions[:,1])
+        ray_directions[:,2] = np.multiply(relative_ray_distance, ray_directions[:,2])
+
+        return ray_directions
+
+    def get_projected_image_corners(self):
+        pass
 
 
 if __name__ == '__main__':
     print("Testing point visibility test")
 
-    T_ball_on_map = [1.0, 0, 0]
-    R_ball_on_map = transforms3d.euler.euler2mat(0.0, 0, 0)
-    A_ball_on_map = transforms3d.affines.compose(T_ball_on_map, R_ball_on_map, np.ones(3))
+    point = np.array([5.5, 3.0])
 
     cam = Camera()
 
-    print(cam.check_if_point_is_visible(A_ball_on_map))
+    print(cam.check_if_point_is_visible(point))
