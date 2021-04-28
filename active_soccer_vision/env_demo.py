8d33e41553a57e5a645fe92c0c2008dbaee20a36
Florian <florian@flova.de>
2021-04-29 00:34:39 +0200

    Update demo env

--- active_soccer_vision/env_demo.py
+++ active_soccer_vision/env_demo.py
@@ -1,12 +1,13 @@
 import time
+import math
 
 from active_soccer_vision.envs import SoccerWorldEnv
 from stable_baselines3.common.env_checker import check_env
 
 env = SoccerWorldEnv()
-#check_env(env)
+check_env(env)
 
 for i in range(8000):
-    env.step([0,0,0,0])
+    print(env.step([-1.0,0.02]))
     env.render()
     time.sleep(env.time_delta)
