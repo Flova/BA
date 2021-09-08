54dacec13627c2bffc22cf785520ba566a2097c0
Florian <florian@flova.de>
2021-09-08 16:11:43 +0200

    Better env demo

--- active_soccer_vision/env_demo.py
+++ active_soccer_vision/env_demo.py
@@ -1,13 +1,14 @@
 import time
 import math
+import numpy as np
 
 from active_soccer_vision.envs import SoccerWorldEnv
 from stable_baselines3.common.env_checker import check_env
 
-env = SoccerWorldEnv()
+env = SoccerWorldEnv("default.yaml")
 check_env(env)
 env.reset()
 
 for i in range(8000):
-    print(env.step([1.0, -0.5]))
+    print(env.step(np.array([0.0, -0.5])))
     env.render()
