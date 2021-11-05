d8c8244baeb55cf278d64e2bc04768fe515f83a6
Florian Vahl <florian@flova.de>
2021-11-05 12:05:24 +0100

    Fix robot loading

--- active_soccer_vision/sim/sim.py
+++ active_soccer_vision/sim/sim.py
@@ -61,12 +61,13 @@ class SoccerWorldSim:
         self.ball = Ball(ball_position_generator, self.time_delta)
 
         # Load and shuffle robots from recording or create a dummy object
-        if self.load_recordings:
+        if self.config['robot']['recorded']:
+            assert len(robot_names) >= self.num_robots, "More robots present in recording than in the config"
             robot_names = self.webots_log_loader.x3d.get_player_names()
             random.shuffle(robot_names)
-            assert len(robot_names) >= self.num_robots, "More robots present in recording than in the config"
+            robot_names = robot_names[:self.num_robots]
         else:
-            robot_names = list(range(5))
+            robot_names = list(range(self.num_robots))
 
         # Create all robots
         self.robots = []
